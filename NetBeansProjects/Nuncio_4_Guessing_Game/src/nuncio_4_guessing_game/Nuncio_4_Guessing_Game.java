dab97f1cdfd1ace9e521fc9a982490222afc0f65
Whovian01 <catrina.nuncio@yahoo.com>
2015-12-16 11:51:47 -0600

    Update Nuncio_4_Guessing_Game.java

--- NetBeansProjects/Nuncio_4_Guessing_Game/src/nuncio_4_guessing_game/Nuncio_4_Guessing_Game.java
+++ NetBeansProjects/Nuncio_4_Guessing_Game/src/nuncio_4_guessing_game/Nuncio_4_Guessing_Game.java
@@ -1,67 +1,200 @@
 
-package nuncio_4_guessing_game;
+/*
 
-public class Nuncio_4_Guessing_Game {
+
+
+* To change this license header, choose License Headers in Project Properties.
+
+
+* To change this template file, choose Tools | Templates
+
+
+* and open the template in the editor.
+
+
+*/
+
+
+package Nuncio_4_GuessingGame;
+
+import java.util.Scanner;
+
+import java.util.Random;
+
+
+
+public class Nuncio_4_GuessingGame {
+
+
+   /**
+
+
+    * @param args the command line arguments
+
+
+    */
+
+
+   static boolean playAgain = true;
+   static String PlayerName;
+
+   static Scanner scanner= new Scanner(System.in);
+
+   public static void main(String[] args) {
+
+    Scanner name = new Scanner(System.in);
+
+    Scanner guess = new Scanner(System.in);
+
+    Random random = new Random();
+
+     int number = random.nextInt(100);
+
 
    
-     static String username;
-     static int userguess;
-     static int magicnumber;
-     static Random  JasonConklinwantsaRandom;
-     static Random randy = new Random ();
-     static boolean playagain;
-    public static void main(String[] args) {
-        playagain = true;
-        magicnumber = randy.nextInt(101);
-        Scanner name = new Scanner(System.in);
-        Scanner guess = new Scanner (System.in);
-       System.out.println("Hello, would you like to play a game?");
-       System.out.println("What is your name?");
-       username = name.nextLine();
-       System.out.println("Hello" + username +
-                "please pick a number between 88 and 202");
-       userguess = guess.nextInt();
-       
-       if (magicnumber == userguess){
-           System.out.println("You win!!!!");
-           playagain = false;
-           
-       } else if (magicnumber > userguess){
-           System.out.println("Too low! Try again.");
-           
-       } else {
-           System.out.println("Too high! Try again.");
-       }    
-       
-         int guessesleft = 0;
-       if (guessesleft < 1 ){
-           System.out.println("NO MORE GUESSES - please play again.");
-           playagain = false;
-           
-       }    
-       Scanner userinputname = new Scanner(System.in);
-       Scanner sc = new Scanner(System.in);
-       //Two main ones we need
-       int something = sc.nextInt();
-       String variable = sc.nextLine();
-       username = userinputname.nextLine();
-       System.out.println("Hello " + username + "Let's play the game.");
-        
-       //runs while the condition is true
-       while(guessesleft > 0 ){
-           playthegame();
-           guessesleft-- ;
+
+
+
+    System.out.println(" Hello! Would would you like to play a game?");
+ 
+
+    System.out.println("What's your name?");
+
+     PlayerName= name.nextLine();
+    
+    System.out.println("Hello " + PlayerName + ".");
+
+
+    while (playAgain = true) {
+
+
+   playthegame();
+
+
+}
+
+
+   }
+
+
+    static void playthegame() {
+
+
+    Scanner guess = new Scanner(System.in);
+
+    Random random = new Random();
+
+    int number = random.nextInt(100);
+
+   int counter = 0;
+  
+
+
+    for (int i = 0; i < 100; i++) {
+
+    System.out.println("Please pick a number.");
+
+    int theGuess = guess.nextInt();
+
+
+
+    if (theGuess == number) {
+
+
+       System.out.println("Congratulations! You win!");
+
+
+       System.out.println(" Guess what? It took you " + counter + " tries!" );
+
+
+       playSomeMore();
+
+
+
+    }
+
+
+
+
+    else if(theGuess < number) {
+
+
+       System.out.println("Number too low. Try again!");  
+
+
+   counter += 1;
+
+
+    }
+
+
+   
+
+
+    else {
+
+
+            System.out.println("Number too high. Please try again.");
+
+
+           counter += 1;
+
+
+    }
+
+
         }
-    }       
 
-    private static void playthegame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+   }
+
+
+    static void playSomeMore() {
+
+
+        System.out.println ("Do you want to play again?");
+
+
+        String yn = scanner.nextLine();
+
+
+
+       if(yn.contains("n")|| yn.contains("N")) {
+
+
+   
+          System.out.println("Okay, bye!");
+
+
+          playAgain = false;
+
+
+       }
+
+
+       else if (yn.contains ("y")|| yn.contains ("Y")){
+
+
+           playthegame();
+
+
+       }
+
+
+       else{
+
+
+   System.out.println("Sorry, I don't understand.");
+
+
+   playSomeMore();
+
+
     }
 
-    
+
     }
-       
-       
-       
-    
+
+
+}
 
